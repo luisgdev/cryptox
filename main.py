0511117fd716af06c3a4cb8c3c2517da0a95058d
Luis Ch <g298luis@gmail.com>
2021-08-02 14:19:31 -0400

    Code improved with Typer

--- main.py
+++ main.py
@@ -1,54 +1,52 @@
+from tinydb import TinyDB, Query
 import requests
-import time
+import typer
+
 
 # SETTINGS
-url = 'https://api.coingecko.com/api/v3'
+app = typer.Typer()
+db = TinyDB('symbols.json')
 
 
-def get_market_cap():
-    endpoint = '/global'
-    # REQUEST DATA
-    response = requests.get(url + endpoint).json()
-    return response['data']['total_market_cap']['usd']
+class Cryptox:
+    def __init__(self, coin):
+        self.url = 'https://api.coingecko.com/api/v3'
+        self.db_path = 'coins.json'
+        self.coin = coin
+
+    def get_coin_list(self):
+        endpoint = '/coins/list'
+        response = requests.get(self.url + endpoint).json()
+        return response
+
+    def get_price(self, ids, vs_currencies):
+        endpoint = f'/simple/price'
+        params = {"ids": ids, "vs_currencies": vs_currencies}
+        response = requests.get(self.url + endpoint, params=params).json()
+        return response[ids][vs_currencies]
+
+    def get_coin_price(self):
+        db_item = db.search(Query().symbol == self.coin)
+        if db_item:
+            data = db_item
+        else:
+            data = self.get_coin_list()
+        for item in data:
+            if self.coin == item['symbol']:
+                print(f'.\nCoingecko ID: {item["id"]}')
+                print(f'Token SYMBOL: {item["symbol"].upper()} ({item["name"]})')
+                print(f' * USD value: {self.get_price(item["id"], "usd")}')
+                print(f' * BTC value: {self.get_price(item["id"], "btc")}')
+                if not db_item:
+                    db.insert(item)
 
 
-def get_coin_list():
-    endpoint = '/coins/list'
-    # REQUEST DATA
-    response = requests.get(url + endpoint).json()
-    return response
-
-
-def get_price(coin, vs_currencies):
-    endpoint = f'/simple/price'
-    params = {"ids": coin, "vs_currencies": vs_currencies}
-    # REQUEST DATA
-    response = requests.get(url + endpoint, params=params).json()
-    return response[coin][vs_currencies]
-
-
-def get_coin_price(symbol):
-    data = get_coin_list()
-    for item in data:
-        if symbol == item['symbol']:
-            print(f'ID: {item["id"]}')
-            print(f'{item["name"]} ({item["symbol"].upper()})')
-            print(f'USD {get_price(item["id"], "usd")}')
-            print(f'BTC {get_price(item["id"], "btc")}')
-
-
-def main():
-    print(f'Crypto Market Cap.: USD {round(get_market_cap(), 2)}')
-    get_coin_price(input('COIN: '))
-    print('Source: Coingecko.com')
+@app.command()
+def main(name: str):
+    crypto = Cryptox(name)
+    typer.echo(crypto.get_coin_price())
+    typer.echo('Source: Coingecko.com')
 
 
 if __name__ == "__main__":
-    # Start counting elapsed time
-    init_time = time.perf_counter()
-    # Do the thing
-    main()
-    # Stop counting elapsed time
-    elapsed = round(time.perf_counter() - init_time, 2)
-    # Show elapsed time
-    print(f' *** Elapsed time: {elapsed} s ***\n.')
+    app()
